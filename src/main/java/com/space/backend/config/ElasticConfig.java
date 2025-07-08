100f8f997d2749354cc58e7211a623fef84a5cdb
user <mse921@naver.com>
2025-07-09 01:01:21 +0900

    commit

--- src/main/java/com/space/backend/config/ElasticConfig.java
+++ src/main/java/com/space/backend/config/ElasticConfig.java
@@ -2,21 +2,12 @@ package com.space.backend.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
-import com.space.backend.resolver.UserSessionResolver;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchClients;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import java.util.List;
@@ -25,10 +16,25 @@ import java.util.List;
 @EnableElasticsearchRepositories(basePackages = "com.space.backend.elastic")
 public class ElasticConfig{
 
-    @Bean
+    /*@Bean
     public RestClient restClient() {
         return RestClient.builder(
                 new HttpHost("localhost", 9200, "http")
+        ).build();
+    }*/
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Value("${elasticsearch.port:9200}")
+    private int esPort;
+
+    @Value("${elasticsearch.scheme:http}")
+    private String esScheme;
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder(
+                new HttpHost(esHost, esPort, esScheme)
         ).build();
     }
 
@@ -38,4 +44,5 @@ public class ElasticConfig{
                 new RestClientTransport(restClient, new JacksonJsonpMapper())
         );
     }
+
 }
